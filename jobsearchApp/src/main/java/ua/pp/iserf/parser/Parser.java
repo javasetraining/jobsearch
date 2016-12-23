78004a0a86f9417e2c12bbffed53c7468ad2267c
code-monster <ua-lex@yandex.ru>
2016-12-23 20:10:50 +0200

    create start point for "parser" project

---
+++ jobsearchApp/src/main/java/ua/pp/iserf/parser/Parser.java
@@ -0,0 +1,17 @@
+
+package ua.pp.iserf.parser;
+
+/**
+ *
+ * @author alex
+ */
+public class Parser {
+    
+    
+    
+        public static void main(String[] args) {
+         
+        System.out.println("Parser start point");
+
+    }
+}
