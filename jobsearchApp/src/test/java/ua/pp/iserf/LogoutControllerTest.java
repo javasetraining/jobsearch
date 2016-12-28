28967077b14c1cc419443a50a18440ce5c2aa1a8
Code <javasetraining@gmail.com>
2016-12-28 12:18:32 +0200

    Test directory added Junit 4.12 update

---
+++ jobsearchApp/src/test/java/ua/pp/iserf/LogoutControllerTest.java
@@ -0,0 +1,16 @@
+package ua.pp.iserf;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Tests for verification Logout LogoutController
+ */
+public class LogoutControllerTest {
+    @Test
+    public void logoutPage() throws Exception {
+
+    }
+
+}
